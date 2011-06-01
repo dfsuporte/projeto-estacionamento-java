c87bca743ed21a88ad612d36b772158e1dcae9c0
andersonsouzars@gmail.com <andersonsouzars@gmail.com@c5308d5a-8b42-8480-6d2f-6b27cfe76fce>
2011-06-01 23:28:59 +0000

    novo projeto

---
+++ trunk/NOVO_PROJETO/src/novo_projeto/NOVO_PROJETO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package novo_projeto;
+
+/**
+ *
+ * @author Anderson
+ */
+public class NOVO_PROJETO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOIII");
+    }
+
+}
