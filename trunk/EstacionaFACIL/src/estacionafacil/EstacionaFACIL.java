965b154fd78fe0b7fbf0fb017a8e3c090eeb481f
andersonsouzars@gmail.com <andersonsouzars@gmail.com@c5308d5a-8b42-8480-6d2f-6b27cfe76fce>
2011-06-02 02:07:37 +0000

    novo projeto

--- trunk/EstacionaFACIL/src/estacionafacil/EstacionaFACIL.java
+++ trunk/EstacionaFACIL/src/estacionafacil/EstacionaFACIL.java
@@ -5,6 +5,7 @@ public class EstacionaFACIL {
     public static void main(String[] args) {
 
         System.out.println("gremiiooooo é o melhor!!!!!");
+        System.out.println("inter o piorrrrr!!!!!");
 
     }
 }
