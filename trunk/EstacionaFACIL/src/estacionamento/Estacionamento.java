fe2d550038b8b59949c0a3cf4727cc07a4ffa303
cipolatti@gmail.com <cipolatti@gmail.com@c5308d5a-8b42-8480-6d2f-6b27cfe76fce>
2011-06-14 03:27:35 +0000

    Criado os pacotes do outro projeto.

---
+++ trunk/EstacionaFACIL/src/estacionamento/Estacionamento.java
@@ -0,0 +1,14 @@
+package estacionamento;
+
+import estacionamento.tela.Tela_Login;
+
+public class Estacionamento {
+
+    public static void main(String[] args) {
+        Tela_Login login = new Tela_Login();
+        login.setLocationRelativeTo(null);
+        login.setVisible(true);
+        
+    }
+
+}
---
+++ trunk/EstacionaFACIL/src/estacionamento/modelo/Login.java
@@ -0,0 +1,31 @@
+package estacionamento.modelo;
+
+public class Login {
+
+    private String usuario;
+    private String senha;
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public boolean isValido() {
+        if (usuario.equals("anderson") && senha.equals("anderson")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
